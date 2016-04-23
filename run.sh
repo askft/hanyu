0abc60399a96990d26d35ad3e350def1cb40955d
Alexander Skafte <alexanderskafte@gmail.com>
2016-04-23 02:31:20 +0200

    Change error for pi2jiu3 and update run script.

--- run.sh
+++ run.sh
@@ -3,7 +3,8 @@
 
 set -e
 
-python3  1_convert.py   words-num.txt   words-acc.txt
-python3  2_collect.py   words-acc.txt   words-nice.txt
-python3  3_practice.py  words-nice.txt
+python3  1_convert.py       words-num.txt    words-acc.txt
+python3  2_collect.py       words-acc.txt    words-nice.txt
+python3  3_practice.py      words-nice.txt
+python3  4_build_table.py   words-nice.txt   words.html
 
